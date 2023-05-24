87809c6e1a50a724b41960d270cb1b896008ad30
kirill.sergeev <kirill.sergeev@progforce.com>
2023-05-24 12:22:13 +0300

    feat: added test jest

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,8 @@
-import express, { Express, Request, Response } from "express";
-import dotenv from "dotenv";
+import dotenv from 'dotenv'
+dotenv.config()
 
-dotenv.config();
+import app from './app'
 
-const app: Express = express();
-const port = process.env.PORT;
+const PORT = process.env.PORT
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Express + TypeScript Server");
-});
-
-app.listen(port, () => {
-  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
-});
+app.listen(PORT, (): void => console.log(`running on port ${PORT}`))
