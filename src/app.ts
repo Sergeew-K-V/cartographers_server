cc51e0c6514ecc25b1a897dadbfd1f41152ac59f
kirill.sergeev <kirill.sergeev@progforce.com>
2023-06-15 12:14:46 +0300

    refactor: added connection to database

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,26 @@
 import express, { Application, Request, Response, NextFunction } from 'express'
-
 import { router as userRoutes } from './routes/user.routes'
+import mongoose from 'mongoose'
 
 const app: Application = express()
 
-app.use('/users', userRoutes)
+async function startServer() {
+  try {
+    if (process.env.DATABASE_URL) {
+      const connection = await mongoose.connect(process.env.DATABASE_URL)
+      connection && console.log('Connected to database')
 
-app.use('/', (req: Request, res: Response, next: NextFunction): void => {
-  res.json({ message: 'Allo! Catch-all route.' })
-})
+      app.use('/users', userRoutes)
+
+      app.use('/', (req: Request, res: Response, next: NextFunction): void => {
+        res.json({ message: 'Allo! Catch-all route.' })
+      })
+    }
+  } catch (error) {
+    console.log('Error!')
+  }
+}
+
+startServer()
 
 export default app
--- src/server.ts
+++ src/server.ts
@@ -5,4 +5,4 @@ import app from './app'
 
 const PORT = process.env.PORT
 
-app.listen(PORT, (): void => console.log(`running on port ${PORT}`))
+app.listen(PORT, (): void => console.log(`Server is running on port ${PORT}`))
